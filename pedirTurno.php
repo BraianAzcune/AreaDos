b48ad4716a90ac90025241ba61580a129c774617
Marcos Abram <marcossebastianabram@gmail.com>
2019-06-20 03:23:16 -0300

    correcion de mensajes de retorno

--- pedirTurno.php
+++ pedirTurno.php
@@ -12,11 +12,22 @@
 
 	$t=new Turno();
 	$u=new Usuario();
-	$mail=$u->getEmail($nombre,$apellido,$contacto);
-	$existe=$t->existeTurno($cancha,$fecha,$horario);
-	if($existe==true) {
-		return -1;
-	} else {
-		return $t->reservarTurno($mail[0][0],$cancha,$fecha,$horario);
+	$resultado= -1;//-
+	$email=$u->getEmail($nombre,$apellido,$contacto);
+	if(!empty($email)){// si no esta vacio, existe el usuario
+		$existe=$t->existeTurno($cancha,$fecha,$horario);
+		if(!$t->existeTurno($cancha,$fecha,$horario)){ //si el turno no exite aun
+			if($t->reservarTurno($email[0][0],$cancha,$fecha,$horario)){
+				$resultado=1;
+			} //lo reservo
+			else{
+				$resultado=0;
+			}
+		}
+		else{
+			$resultado=0;
+		}
+
 	}
+	echo $resultado //-1 signidica que el usuario no existe, 0 el turno no esta disponible o error al cargar, 1 se cargo con exito
 ?>
